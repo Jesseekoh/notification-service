72329a26240760b3489ef80c39373ba924ae62e9
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2026-09-15 15:28:58 +0100

    cleanup

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module.js';
+import { TemplatesModule } from './templates/templates.module.js';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TemplatesModule],
 })
 export class AppModule { }
