7c6d7f70ab74f63f99b2334c8d5690fa5c916a2d
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2026-09-15 22:19:24 +0100

    add exception filter to the api-gateway

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module.js';
 import { TemplatesModule } from './templates/templates.module.js';
+import { NotificationModule } from './notification/notification.module.js';
+import { APP_FILTER } from '@nestjs/core';
+import { SharedRpcToHttpExceptionFilter } from '@notification/common';
 
 @Module({
-  imports: [UsersModule, TemplatesModule],
+  imports: [UsersModule, TemplatesModule, NotificationModule],
+  providers: [
+    { provide: APP_FILTER, useClass: SharedRpcToHttpExceptionFilter },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -12,7 +12,6 @@ async function bootstrap() {
     }),
   );
 
-  app.useGlobalFilters(new SharedRpcToHttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
